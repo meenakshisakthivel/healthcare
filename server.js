7b732427ac1d2e7f2cd2308128a60207cfd153f3
Meenakshi Sakthivel <meenakshisakthivel@users.noreply.github.com>
2026-09-19 09:29:10 +0530

    Add Vercel deployment configuration

--- server.js
+++ server.js
@@ -527,6 +527,10 @@ app.use((err, req, res, next) => {
   next();
 });
 
-app.listen(PORT, () => {
-  console.log(`Cauvery Med server running at http://localhost:${PORT}`);
-});
+if (require.main === module) {
+  app.listen(PORT, () => {
+    console.log(`Cauvery Med server running at http://localhost:${PORT}`);
+  });
+}
+
+module.exports = app;
